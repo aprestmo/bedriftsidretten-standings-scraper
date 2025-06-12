e6e0b994173d621b2f9a9fb29b0edeffcbc8af69
Alexander Prestmo <aprestmo@gmail.com>
2025-06-12 22:54:46 +0200

    feat(app): Update package refs

--- scrape.js
+++ scrape.js
@@ -1,6 +1,6 @@
 const { chromium } = require('playwright');
-const fs = require('fs');
-const path = require('path');
+const fs = require('node:fs');
+const path = require('node:path');
 
 (async () => {
   const browser = await chromium.launch();
